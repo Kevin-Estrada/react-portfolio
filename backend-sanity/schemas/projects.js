6d2b8ac4f4d4a31471ab2addf32216c77f9b4732
Kevin Estrada <ke81297@gmail.com>
2022-03-21 23:04:01 -0500

    Adding more schemas as suited for the portfolio layout later on

--- backend-sanity/schemas/projects.js
+++ backend-sanity/schemas/projects.js
@@ -4,22 +4,46 @@ export default {
   type: 'document',
   fields: [
     {
-      name: 'name',
-      title: 'Name',
+      name: 'title',
+      title: 'Title',
       type: 'string',
     },
+
     {
       name: 'description',
       title: 'Description',
       type: 'string',
     },
     {
-      name: 'imageurl',
-      title: 'Image',
+      name: 'projectLink',
+      title: 'Project Link',
+      type: 'string',
+    },
+    {
+      name: 'codeLink',
+      title: 'Code Link',
+      type: 'string',
+    },
+    {
+      name: 'imgUrl',
+      title: 'ImageUrl',
       type: 'image',
       options: {
         hotspot: true,
       },
+    },
+
+    {
+      name: 'tags',
+      title: 'Tags',
+      type: 'array',
+      of: [
+        {
+          name: 'tag',
+          title: 'Tag',
+          type: 'string',
+        },
+      ],
     },
   ],
 };
