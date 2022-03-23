a0196ac618f29ad2f53ed6ebce62230efd01464e
Kevin Estrada <ke81297@gmail.com>
2022-03-22 21:59:00 -0500

    Just added the about section that is connected with the sanity client to fetch the documents from CMS

--- frontend-react/src/container/About/About.jsx
+++ frontend-react/src/container/About/About.jsx
@@ -3,31 +3,40 @@ import { motion } from 'framer-motion';
 
 import { images } from '../../constants';
 import './About.scss';
+import { urlFor, client } from '../../client';
 
-const abouts = [
-  {
-    title: 'Web Development',
-    description: 'I am a good web developer',
-    imgUrl: images.about01,
-  },
-  {
-    title: 'Web Design',
-    description: 'I am a good web developer',
-    imgUrl: images.about02,
-  },
-  {
-    title: 'UI/UX',
-    description: 'I am a good web developer',
-    imgUrl: images.about03,
-  },
-  {
-    title: 'Full Stack',
-    description: 'I am a good web developer',
-    imgUrl: images.about04,
-  },
-];
+// const abouts = [
+//   {
+//     title: 'Web Development',
+//     description: 'I am a good web developer',
+//     imgUrl: images.about01,
+//   },
+//   {
+//     title: 'Web Design',
+//     description: 'I am a good web developer',
+//     imgUrl: images.about02,
+//   },
+//   {
+//     title: 'UI/UX',
+//     description: 'I am a good web developer',
+//     imgUrl: images.about03,
+//   },
+//   {
+//     title: 'Full Stack',
+//     description: 'I am a good web developer',
+//     imgUrl: images.about04,
+//   },
+// ];
 
 const About = () => {
+  const [abouts, setAbouts] = useState([]);
+
+  useEffect(() => {
+    const query = '*[_type == "abouts"]';
+
+    client.fetch(query).then((data) => setAbouts(data));
+  }, []);
+
   return (
     <>
       <h2 className="head-text">
@@ -44,7 +53,7 @@ const About = () => {
             className="app__profile-item"
             key={about.title + index}
           >
-            <img src={about.imgUrl} alt={about.title} />
+            <img src={urlFor(about.imgUrl)} alt={about.title} />
             <h2 className="bold-text" style={{ marginTop: 20 }}>
               {about.title}
             </h2>
